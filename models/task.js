7d87540c7bd5bf05e4f056c90c68ba5712f8518d
justzxid <zaidmk96@gmail.com>
2025-03-02 14:58:27 +0300

    Updated Model categories

--- models/task.js
+++ models/task.js
@@ -25,7 +25,7 @@ const taskSchema = new mongoose.Schema(
         category: {
             type: String,
             required: true,
-            enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
+            enum: ['Not started', 'In process', 'Done',],
         },
         author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         comments: [commentSchema],
